349a1c30355f415640795789e91a6f90956a6f98
AlfGoto <147511905+AlfGoto@users.noreply.github.com>
2024-05-21 10:19:05 +0200

    added: Nav is created

--- cms/app/layout.js
+++ cms/app/layout.js
@@ -1,22 +1,30 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "AlfCMS",
-  description: "The simplest CMS ever",
+    title: "AlfCMS",
+    description: "The simplest CMS ever",
 };
 
+function Nav() {
+    return (<nav>
+        <Link href='/'>Home</Link>
+        <Link href='/log'>Login</Link>
+    </nav>)
+}
+
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div id="left"></div>
-        <div id="center">{children}</div>
-        <div id="right"></div>
-        </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <div id="left"><Nav /></div>
+                <div id="center">{children}</div>
+                <div id="right"></div>
+            </body>
+        </html>
+    );
 }
